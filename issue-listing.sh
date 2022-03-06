a082d62e4854f9ae15b3efe60b668df8c506a489
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-03-06 14:41:51 -0300

    Init

---
+++ issue-listing.sh
@@ -0,0 +1,10 @@
+cd /media/daniell/Windows\ 10\ main/Users/Daniell/Desktop/Orgute/peeper-user-daniell-mesquita/.issues/open
+#ls -f
+#ls -h
+#ls --file-type '.json'
+#ls --file-type '*.json'
+#ls --file-type *.json
+#ls --file-type *.json > test.txt
+#ls --file-type *.json | sort -nk1.8
+#ls --file-type *.json | sort -t_ -nk2
+ls --file-type *.json | sort -n #credits: https://www.unix.com/302856521-post4.html
