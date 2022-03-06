d9357d0bf0dc836ebf0e916de6529ddd3ce5dc58
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-03-06 17:55:55 -0300

    List closed issues; use the same function for processing both open and closed issues

--- issue-listing.sh
+++ issue-listing.sh
@@ -1,35 +1,31 @@
 #!/bin/bash -e
 
-cd /media/daniell/Windows\ 10\ main/Users/Daniell/Desktop/Orgute/peeper-user-daniell-mesquita/.issues/open
+#cd "/media/daniell/Windows\ 10\ main/Users/Daniell/Desktop/Orgute/peeper-user-daniell-mesquita/.issues"
+cd /media/daniell/Windows\ 10\ main/Users/Daniell/Desktop/Orgute/peeper-user-daniell-mesquita/.issues
 
-tmp="$(ls --file-type *.json | sort -n)" #- credits: https://www.unix.com/302856521-post4.html
-echo "$tmp" > tmp.txt.tmp
-#echo $tmp > tmp.txt
-#tmp2="$(cat tmp.txt)"
-#tmp2="$(echo $tmp)"
-#rm tmp.txt
-#formated="${tmp2%.*}"
-
-loopmile="0"
-
-cd ..
-#if [ "$(jq -r '.open' issues-list.json)" = "null" ] || [ "$(jq -r '.open' issues-list.json)" = "" ]; then
-#   echo "Its your first time using issue-listing.sh!"
-#   echo "Initializing..."
-#   cat >> issues-list.json << ENDOFFILE
-#{
-#	"open": [{}],
-#	"closed": [{}]
-#}
-#ENDOFFILE
-#fi
 if [ "$(jq -r '.open' data.json)" = "null" ] || [ "$(jq -r '.open' data.json)" = "" ]; then
    echo "Its your first time using issue-listing.sh!"
    echo "Initializing..."
    tmp="$(mktemp)"; cat data.json | jq ". += {\"open\":[{}]}" >"$tmp" && mv "$tmp" data.json
    tmp="$(mktemp)"; cat data.json | jq ". += {\"closed\":[{}]}" >"$tmp" && mv "$tmp" data.json
 fi
-cd open
+
+process_issues () {
+#if [ "$currenttype" = "open" ]; then
+#   
+#fi
+#if [ "$currenttype" = "closed" ]; then
+#   
+#fi
+
+#cd /media/daniell/Windows\ 10\ main/Users/Daniell/Desktop/Orgute/peeper-user-daniell-mesquita/.issues/$currenttype
+#cd "/media/daniell/Windows\ 10\ main/Users/Daniell/Desktop/Orgute/peeper-user-daniell-mesquita/.issues/$currenttype"
+cd "$currenttype"
+
+tmp="$(ls --file-type *.json | sort -n)" #- credits: https://www.unix.com/302856521-post4.html
+echo "$tmp" > tmp.txt.tmp
+
+loopmile="0"
 
 while IFS="" read -r p || [ -n "$p" ]
 do
@@ -37,14 +33,25 @@ do
   formated="${p%.*}"
   echo "$formated"
   #tmp="$(mktemp)"; cat issues-list.json | jq ".open[] += {\"$loopmile\":[{\"id\": \"\",\"metadata\":\"\",\"name\":\"\",\"description\":\"\",\"image\":\"\",\"blockchain\":\"\"}]}" >"$tmp" && mv "$tmp" issues-list.json
-  tmp="$(mktemp)"; cat data.json | jq ".open[] += {\"$loopmile\":\"$formated\"}" >"$tmp" && mv "$tmp" data.json
+  tmp="$(mktemp)"; cat data.json | jq ".\"$currenttype\"[] += {\"$loopmile\":\"$formated\"}" >"$tmp" && mv "$tmp" data.json
   #printf '%s\n' "$p"
   loopmile="$(($loopmile + 1))"
   echo "Loop: $loopmile"
-  cd open
+  cd "$currenttype"
+  #rm tmp.txt.tmp
+  #cd ..
 done < tmp.txt.tmp
+#loopmile="0"
 #- credits: https://stackoverflow.com/a/1521498/5623661
-
+#cd "$currenttype"
 rm tmp.txt.tmp
+cd ..
 
-#echo "$formated"
+#rm tmp.txt.tmp
+
+}
+
+currenttype="open"
+process_issues
+currenttype="closed"
+process_issues
