36c1c0b497effa6fdd10b5ebfa3835fee5d55f9b
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-03-06 16:32:57 -0300

    Successfully remove ".json" from issues list

--- issue-listing.sh
+++ issue-listing.sh
@@ -1,20 +1,23 @@
-cd /media/daniell/Windows\ 10\ main/Users/Daniell/Desktop/Orgute/peeper-user-daniell-mesquita/.issues/open
-#ls -f
-#ls -h
-#ls --file-type '.json'
-#ls --file-type '*.json'
-#ls --file-type *.json
-#ls --file-type *.json > test.txt
-#ls --file-type *.json | sort -nk1.8
-#ls --file-type *.json | sort -t_ -nk2
-#ls --file-type *.json | sort -n #credits: https://www.unix.com/302856521-post4.html
-
 #!/bin/bash -e
 
-tmp="$(ls --file-type *.json | sort -n)"
-#echo "$tmp" > tmp.txt
+cd /media/daniell/Windows\ 10\ main/Users/Daniell/Desktop/Orgute/peeper-user-daniell-mesquita/.issues/open
+
+tmp="$(ls --file-type *.json | sort -n)" #- credits: https://www.unix.com/302856521-post4.html
+echo "$tmp" > tmp.txt.tmp
 #echo $tmp > tmp.txt
 #tmp2="$(cat tmp.txt)"
-tmp2="$(echo $tmp)"
+#tmp2="$(echo $tmp)"
 #rm tmp.txt
-formated="${tmp2%.*}"
+#formated="${tmp2%.*}"
+
+while IFS="" read -r p || [ -n "$p" ]
+do
+  formated="${p%.*}"
+  echo "$formated"
+  #printf '%s\n' "$p"
+done < tmp.txt.tmp
+#- credits: https://stackoverflow.com/a/1521498/5623661
+
+rm tmp.txt.tmp
+
+#echo "$formated"
